409918c06ad1fb2e0e639cd70ca7ebc7f15556b3
lucia1 <lucia1@pulmuone.com>
2023-02-28 13:56:40 +0900

    [section4]데코레이터패턴-예제1,2,3/정리

---
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,43 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    /**
+     * client -> realComponent 의존관계 설정
+     * */
+    @Test
+    void noDecorator(){
+        Component realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    /**
+     * client -> messageDecorator -> realComponent 객체 의존관계 설정
+     * */
+    @Test
+    void decorator1(){
+        Component realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    /**
+     * client -> timeDecorator -> messageDecorator -> realComponent 객체 의존관계 설정
+     * */
+    @Test
+    void decorator2(){
+        Component realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
+
+}
